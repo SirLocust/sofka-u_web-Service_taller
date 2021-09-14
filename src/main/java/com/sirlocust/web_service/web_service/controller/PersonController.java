9d3b5a760bfa5e9f7f16760f018c1d1b5e5b42aa
sirlocust <jua56yu@hotmail.com>
2021-09-13 23:10:19 -0500

    se agregan los responseEntity

--- src/main/java/com/sirlocust/web_service/web_service/controller/PersonController.java
+++ src/main/java/com/sirlocust/web_service/web_service/controller/PersonController.java
@@ -9,8 +9,10 @@ import com.sirlocust.web_service.web_service.service.IPersonService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,13 +62,19 @@ public class PersonController {
     ;
   }
 
-  @DeleteMapping(value = "removePerson/{id}")
-  public ResponseEntity<HttpStatus> removePerson(@PathVariable() int id) {
-    if (servicePerson.delete(id)) {
-      return new ResponseEntity<>(HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  @DeleteMapping(value = "person/{id}")
+  public ResponseEntity<Map<String, Object>> removePerson(@PathVariable() Long id) {
+    Person personDb = this.personService.delete(id);
+    Map<String, Object> body = new HashMap<>();
+    body.put("data", personDb);
+    if (personDb == null) {
+      body.put("message", "Persona No Eliminada");
+      body.put("errors", "Figura no existe");
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
+    body.put("message", "Persona Eliminada con exito");
+    body.put("errors", null);
+    return ResponseEntity.status(HttpStatus.OK).body(body);
   }
 
 }
